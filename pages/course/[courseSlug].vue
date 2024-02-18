b28c8acbefaab16447fcb0b238dd0dc27b272a6e
hjkang94 <hjkang94@github.com>
2024-02-18 16:18:30 +0900

    NuxtLink prefetching 테스트

--- pages/course/[courseSlug].vue
+++ pages/course/[courseSlug].vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="q-pa-xl">
+  <div>
     <AppCard>
       <template #header>
         <div class="text-h5 text-weight-medium">{{ course?.title }}</div>
@@ -45,6 +45,46 @@
       <p class="q-mt-lg text-grey-8">
         {{ course?.content }}
       </p>
+      <q-separator class="q-mb-lg" />
+      <q-form class="q-gutter-y-md">
+        <q-btn
+          label="수강완료"
+          class="full-width"
+          color="green"
+          unelevated
+          :outline="completed ? false : true"
+          :icon="completed ? 'check' : undefined"
+          @click="completed = !completed"
+        />
+        <q-input
+          v-model="memo"
+          type="textarea"
+          outlined
+          dense
+          placeholder="메모를 작성해주세요."
+          rows="3"
+          autogrow
+        />
+      </q-form>
+      <template #footer?>
+        <ClientOnly>
+          <q-btn
+            v-if="prevCourse"
+            label="이전 강의"
+            color="primary"
+            unelevated
+            :to="prevCourse.path"
+          />
+          <q-space />
+          <q-btn
+            v-if="nextCourse"
+            label="다음 강의"
+            color="primary"
+            unelevated
+            :to="nextCourse.path"
+          />
+        </ClientOnly>
+      </template>
     </AppCard>
   </div>
 </template>
@@ -52,7 +92,17 @@
 <script setup lang="ts">
 const route = useRoute();
 const courseSlug = route.params.courseSlug as string;
-const { course } = useCourse(courseSlug);
+const { course, prevCourse, nextCourse } = useCourse(courseSlug);
+
+definePageMeta({
+  key: (route) => route.fullPath,
+  title: 'My HomePage',
+  pageType: '',
+  keepalive: true,
+});
+
+const memo = ref('');
+const completed = ref(false);
 </script>
 
 <style scoped></style>
---
+++ pages/course/prefetching-2.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <div class="text-denter q-py-xl">
+      <div class="text-h4">
+        {{ $route.path }}
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+console.log('prefetching-2');
+</script>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
